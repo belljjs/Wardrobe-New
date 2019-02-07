f5f52dfc2bcba15651fdf8e0c8f6faa56ea12d4b
belliis <belljjs@gmail.com>
2019-02-06 20:33:40 -0800

    Create Items

---
+++ client/src/Items/Items.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Item from '../Item/Item';
+import './Items.css'
+import '../global.css';
+
+
+
+
+const items = (props) => {
+
+    const itemsShown = [...props.itemsShown]
+    const items = itemsShown.map( ({id, category, color, season, name},index) => 
+            <Item 
+                key={id}
+                id={id}
+                category={category}
+                color={color}
+                season={season}
+                name ={name}
+                clicked={props.itemsClicked.bind(this, index)}> {name}
+            </Item>
+    )
+    return(
+        <div>
+            <p className="subTitle">ITEMS </p> 
+            <div className="Items">
+                {items}
+            </div>
+        </div>
+        
+    )
+}
+
+
+ 
+
+
+export default items;
