a53298a679e40ec3222b6aaf3e5cdcc973fd9814
belliis <belljjs@gmail.com>
2019-02-07 21:47:12 -0800

    install axios

--- client/src/AddItem/AddItem.js
+++ client/src/AddItem/AddItem.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
+import Button from '../UI/Button/Button';
 
 
 class AddItem extends Component {
     state = {
-        proposal :[]
+        proposal :[],
+        submitted: false
     }
+    postDataHandler = ()=>{
+        this.setState({submitted: true})
+
+        // const data = {
+        //     title: this.state.title,
+        //     body: this.state.content,
+        //     author: this.state.author,
+        // }
+        // axios.post('https://jsonplaceholder.typicode.com/posts',data)
+        // .then(res=>{
+        //     console.log(res);
+        //     this.setState({submitted: true})
+        //     //this.props.history.push('/Posts');
+        // })
+    }
+
     render() {
+        let redirect = null;
+        if (this.state.submitted){
+            redirect = <Redirect to='/closet'/>
+        }
         return (
             <div>
+                {redirect}
                 <h3 className="title">Add Item</h3>
+                <Button clicked={this.postDataHandler}> SUBMIT </Button>
             </div>
         );
     }
