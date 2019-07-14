182a9c337ef3f119e7f2827fd318786329a3863e
belliis <belljjs@gmail.com>
2019-07-13 18:52:55 -0700

    install material-UI

--- client/src/containers/Guest/Guest.js
+++ client/src/containers/Guest/Guest.js
@@ -20,11 +20,21 @@ import './Guest.css';
 const guest =() => {
     return (
         <div>
-            <h3 className="titleHome"> Try Wardrobe as guest with sample data </h3>
+            <h3 className="titleGuest"> Try Wardrobe as guest with sample data </h3>
             <div>
                 <div className= "titlePage"> Check Weather </div>
-
-                    <p>Then, the menu bar shows available links.</p>
+                <p>To make an outfit for today, check weather first.</p>
+                <p>Choose city in sample cities or add some new citiies.</p>
+                <p>You can use current location.</p>
+            </div>
+            <div>
+                <div className= "titlePage"> Add and Delete items </div>
+                <p>Add new item with a picture. You can delete them</p>
+            </div>
+            <div>
+                <div className= "titlePage"> Open your closet and try outfits </div>
+                <p>Select any combination and see it bigger with magnifier.</p>
+                <p>If it looks nice, save as your new outfit</p>
             </div>
             <div>
 
